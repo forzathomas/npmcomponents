9cf00833dc117191bb595458360289ea260d67f5
Thomas Machiels <forzathomassos@gmail.com>
2017-10-26 11:01:52 +0200

    added a new button

--- EpicButton3.js
+++ EpicButton3.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { Button } from "react-native"
 
-class EpicButton extends Component {
+class EpicButton3 extends Component {
     render() {
         const {onPress} = this.props
         return (
-            <Button title="Knop" onPress={onPress}/>
+            <Button title="Dit is de 3de knop" onPress={onPress}/>
         )
     }
 }
 
-export default EpicButton
+export default EpicButton3
