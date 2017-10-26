d4cf9e9286472f1a28f17298c8f52b9ac05593bf
Thomas Machiels <forzathomassos@gmail.com>
2017-10-26 14:28:22 +0200

    fix: added 3de

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import EpicButton from './EpicButton'
 import EpicButton2 from './EpicButton2'
+import EpicButton3 from './EpicButton3'
 
 exports.EpicButton = EpicButton
 exports.EpicButton2 = EpicButton2
