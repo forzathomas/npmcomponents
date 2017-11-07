feacd25bf29379fe7898b5af848c00623f8582d1
forzathomas <thomas-machiels@hotmail.com>
2017-11-07 15:29:05 +0100

    added comments

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import EpicButton from './EpicButton'
 import EpicButton2 from './EpicButton2'
 import EpicButton3 from './EpicButton3'
 
+//added some comments here
+
 exports.EpicButton = EpicButton
 exports.EpicButton2 = EpicButton2
 exports.EpicButton3 = EpicButton3
