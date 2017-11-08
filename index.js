caca18c6e9c93b82d5e0028bfbc598e56cd21e7d
forzathomas <thomas-machiels@hotmail.com>
2017-11-08 09:28:16 +0100

    deleted comments

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ import EpicButton from './EpicButton'
 import EpicButton2 from './EpicButton2'
 import EpicButton3 from './EpicButton3'
 
-//added some comments here
-
-//added some more comments
-
-//even more comments x2
-
-//bal bla bla test test more
-
 exports.EpicButton = EpicButton
 exports.EpicButton2 = EpicButton2
 exports.EpicButton3 = EpicButton3
